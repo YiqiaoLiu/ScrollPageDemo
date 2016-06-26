0a70be4268bb12b55644c87f9c61a8c4adac2712
yiqiaoliu@virtroid.com <yiqiaoliu@virtroid.com>
2016-06-26 15:32:18 -0700

    Add a simple scroll page, changed based on which page is closest to the drag end position

---
+++ ScrollPage/Assets/ScrollController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+public class ScrollController : MonoBehaviour, IBeginDragHandler, IEndDragHandler {
+
+    private int pageCount = 0;
+    private ScrollRect rect;
+    private bool isDrag = false;
+    private float[] pages = { 0, 0.25f, 0.5f, 0.75f, 1f };
+
+	// Use this for initialization
+	void Start () {
+        pageCount = this.transform.childCount;
+        rect = GetComponent<ScrollRect>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    public void OnBeginDrag(PointerEventData data) {
+        isDrag = true;
+    }
+
+    public void OnEndDrag(PointerEventData data) {
+        isDrag = false;
+
+        //Get the current scroll page offset
+        float posHorizontal = rect.horizontalNormalizedPosition;
+
+        //This is the check page index
+        int pageIndex = 0;
+
+        //Caculating the offset between the page 0 and the current pos
+        float dragOffset = Mathf.Abs(pages[pageIndex] - posHorizontal);
+
+        //Caculating the offset between each page and the current pos
+        //Chosing the closest page as the next frame changing page
+        for (int i = 1; i < 5; i++) {
+            float tempOffset = Mathf.Abs(pages[i] - posHorizontal);
+            if (tempOffset < dragOffset) {
+                dragOffset = tempOffset;
+                pageIndex = i;
+            }
+
+            rect.horizontalNormalizedPosition = pages[pageIndex];
+        }
+    }
+}
